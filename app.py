a60b1e55718b0b6fdba349a24e6b7ffd3a39771a
pedrohbsilva <phbs1235@gmail.com>
2022-09-14 02:48:18 -0300

    testes na gcp

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
+import os
 from flask import Flask
 app = Flask(__name__)
+
 @app.route('/')
 def hello():
 	return "Hello world"
+
+if __name__ == "__main__":
+  app.run(port=int(os.environ.get("PORT", 8080)), host='0.0.0.0',debug=False)
