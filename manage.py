d6adcc09ed2ad0b33febf765b05b8d96d4628ba8
pedrohbsilva <phbs1235@gmail.com>
2022-09-14 02:13:46 -0300

    testes na gcp

--- manage.py
+++ manage.py
@@ -33,4 +33,4 @@ def populate_db():
   print('Success')
      
 if __name__ == "__main__":
-  manager.run(port=int(os.environ.get("PORT", 5000)), host='0.0.0.0',debug=False)
+  manager.run(port=int(os.environ.get("PORT", 8080)), host='0.0.0.0',debug=False)
