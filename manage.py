1369df853607e0d2429e202511352a00e5935b2d
pedrohbsilva <phbs1235@gmail.com>
2022-09-14 02:06:24 -0300

    testes na gcp

--- manage.py
+++ manage.py
@@ -1,3 +1,4 @@
+import os
 from flask import jsonify
 import requests
 from app import create_app, db
@@ -32,4 +33,4 @@ def populate_db():
   print('Success')
      
 if __name__ == "__main__":
-  manager.run()
+  manager.run(port=int(os.environ.get("PORT", 5000)), host='0.0.0.0',debug=False)
